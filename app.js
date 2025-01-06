5a158be947df9b2fcc523a35885c4a3ba5b817d8
Neville Jeremy <ketjeh.developer@gmail.com>
2025-01-06 18:02:53 +0700

    fix: fix static files serving

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const upload = multer({ storage }); // Menetapkan konfigurasi penyimpanan
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 // Middleware for auth protection
